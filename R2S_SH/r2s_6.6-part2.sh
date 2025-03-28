57e4bccc2dac4cf3ba2f20b9f63199f84ec1a322
churricane <38831053+churricane@users.noreply.github.com>
2025-03-28 14:28:38 +0800

    Update r2s_6.6-part2.sh

--- R2S_SH/r2s_6.6-part2.sh
+++ R2S_SH/r2s_6.6-part2.sh
@@ -54,8 +54,8 @@ rm -rf ./feeds/luci/applications/luci-app-netdata/
 git clone https://github.com/sirpdboy/luci-app-netdata feeds/luci/applications/luci-app-netdata
 
 #update go-lang 1.23
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
 #如果Lean的源码出错，lede根目录下，删除多余的
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+#rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
