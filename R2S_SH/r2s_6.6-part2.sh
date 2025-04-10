f33ae8273069f19b157176a4d72846bb0a22e2ab
churricane <38831053+churricane@users.noreply.github.com>
2025-04-10 23:00:08 +0800

    Update r2s_6.6-part2.sh

--- R2S_SH/r2s_6.6-part2.sh
+++ R2S_SH/r2s_6.6-part2.sh
@@ -11,22 +11,25 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' ./package/base-files/files/bin/config_generate
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/P3TERX-Router/g' ./package/base-files/files/bin/config_generate
 
 #ssh bash
-sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
+sed -i 's/\/bin\/ash/\/bin\/bash/' ./package/base-files/files/etc/passwd
 
 #empty password
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" ./package/lean/default-settings/files/zzz-default-settings
+
+#kernel 5.15
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' target/linux/rockchip/Makefile
 
 #cgo enable
-sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/g' feeds/packages/utils/v2dat/Makefile
+#sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/g' feeds/packages/utils/v2dat/Makefile
 
 #remove defaut package
 sed -i 's/luci-app-accesscontrol//g' include/target.mk
@@ -41,21 +44,21 @@ sed -i 's/luci-app-vsftpd//g' include/target.mk
 
 #取消Nginx强制转跳https，取消Nginx对登录地址限制
 #取消登录IP限制
-sed -i "2i sed -i '/restrict_locally/d' /etc/config/nginx" package/lean/default-settings/files/zzz-default-settings
+sed -i "2i sed -i '/restrict_locally/d' /etc/config/nginx" ./package/lean/default-settings/files/zzz-default-settings
 #取消强制转跳https
-sed -i "3i sed -i '/302/d' /etc/config/nginx" package/lean/default-settings/files/zzz-default-settings
+sed -i "3i sed -i '/302/d' /etc/config/nginx" ./package/lean/default-settings/files/zzz-default-settings
 #设置80端口加载.locations
-sed -i "4i uci add_list nginx._redirect2ssl.include='conf.d/*.locations'" package/lean/default-settings/files/zzz-default-settings
+sed -i "4i uci add_list nginx._redirect2ssl.include='conf.d/*.locations'" ./package/lean/default-settings/files/zzz-default-settings
 #设置Nginx配置生效
-sed -i '5i uci commit nginx' package/lean/default-settings/files/zzz-default-settings
+sed -i '5i uci commit nginx' ./package/lean/default-settings/files/zzz-default-settings
 
 #netdata chienese 
-rm -rf ./feeds/luci/applications/luci-app-netdata/  
-git clone https://github.com/sirpdboy/luci-app-netdata feeds/luci/applications/luci-app-netdata
+#rm -rf ./feeds/luci/applications/luci-app-netdata/  
+#git clone https://github.com/sirpdboy/luci-app-netdata feeds/luci/applications/luci-app-netdata
 
 #update go-lang 1.23
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
 #如果Lean的源码出错，lede根目录下，删除多余的
-#rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
