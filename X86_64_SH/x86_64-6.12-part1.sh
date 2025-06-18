83fd41e0f0416b449959fa25f51e78cfe8125c7a
churricane <churricane@gmail.com>
2025-06-18 09:45:13 +0800

    add kernel 6.12

---
+++ X86_64_SH/x86_64-6.12-part1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#check kernel 6.6.90
+#git checkout b3f63c7948b4b59df5f837b9e3bc343c22f74798
+#check kernel 6.6.91
+#git checkout 6620a5e83caca39793b72e9a531a0d73e9ee3b67
+#check kernel 6.6.92
+#git checkout 6a5bb9d211ec45718d4956dd3deb976aa07e79fe
+
+# Add a feed source
+echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+#echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
---
+++ X86_64_SH/x86_64-6.12-part2.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+# img add build date
+sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+#ssh bash
+sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
+
+#empty password
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+
+#cgo enable
+#sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/g' feeds/packages/utils/v2dat/Makefile
+
+#remove defaut package
+sed -i 's/luci-app-ddns//g; s/luci-app-upnp//g; s/ddns-scripts_aliyun//g; s/ddns-scripts_dnspod//g; s/luci-app-vsftpd//g; s/luci-app-vlmcsd//g; s/luci-app-wol//g; s/luci-app-accesscontrol//g; s/luci-app-filetransfer//g' include/target.mk
+#kernel 6.6.36
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.12/KERNEL_PATCHVER:=6.6/g' target/linux/x86/Makefile
+#取消Nginx强制转跳https，取消Nginx对登录地址限制
+#取消登录IP限制
+sed -i "2i sed -i '/restrict_locally/d' /etc/config/nginx" package/lean/default-settings/files/zzz-default-settings
+#取消强制转跳https
+sed -i "3i sed -i '/302/d' /etc/config/nginx" package/lean/default-settings/files/zzz-default-settings
+#设置80端口加载.locations
+sed -i "4i uci add_list nginx._redirect2ssl.include='conf.d/*.locations'" package/lean/default-settings/files/zzz-default-settings
+#设置Nginx配置生效
+sed -i '5i uci commit nginx' package/lean/default-settings/files/zzz-default-settings
+
+#netdata chienese 
+#rm -rf ./feeds/luci/applications/luci-app-netdata/  
+#git clone https://github.com/sirpdboy/luci-app-netdata feeds/luci/applications/luci-app-netdata
+
+#update go-lang 1.23
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+
+#如果Lean的源码出错，lede根目录下，删除多余的
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
